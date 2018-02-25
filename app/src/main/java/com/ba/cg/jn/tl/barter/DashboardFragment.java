a6f76c66b3656e427a091804ce5b9723c5154ef7
Tiffany Lui <tiffany.lui621@gmail.com>
2018-02-24 22:12:29 -0500

    Inital app commit

---
+++ app/src/main/java/com/ba/cg/jn/tl/barter/DashboardFragment.java
@@ -0,0 +1,71 @@
+package com.ba.cg.jn.tl.barter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DashboardFragment extends Fragment {
+
+    private OnDashboardActionSelected mCallback;
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.dashboard_fragment, container, false);
+
+        TextView userGreeting = v.findViewById(R.id.user_greeting);
+        userGreeting.setText(getString(R.string.user_greeting, FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+
+        Button signOut = v.findViewById(R.id.sign_out);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.callbackSignOut();
+            }
+        });
+
+        Button deleteAccount = v.findViewById(R.id.delete_account);
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.callbackDeleteAccount();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnDashboardActionSelected) {
+            mCallback = (OnDashboardActionSelected) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnDashboardActionSelected");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallback = null;
+    }
+
+    public interface OnDashboardActionSelected {
+        void callbackSignOut();
+
+        void callbackDeleteAccount();
+    }
+}
---
+++ app/src/main/java/com/ba/cg/jn/tl/barter/MainActivity.java
@@ -0,0 +1,113 @@
+package com.ba.cg.jn.tl.barter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DashboardFragment.OnDashboardActionSelected {
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startDashboardFrag();
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            signInUi();
+        } else {
+            startDashboardFrag();
+        }
+    }
+
+    private void startDashboardFrag() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment dashBoardFrag = new DashboardFragment();
+
+        fragmentTransaction.add(R.id.fragment_container, dashBoardFrag);
+        fragmentTransaction.commit();
+    }
+
+    private void signInUi() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                , RC_SIGN_IN);
+    }
+
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(getApplicationContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        signInUi();
+                    }
+                });
+    }
+
+    private void deleteAccount() {
+        AuthUI.getInstance().delete(getApplicationContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        signInUi();
+                    }
+                });
+    }
+
+    @Override
+    public void callbackSignOut() {
+        signOut();
+    }
+
+    @Override
+    public void callbackDeleteAccount() {
+        deleteAccount();
+    }
+}
