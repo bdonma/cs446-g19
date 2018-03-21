9ef25dfae329628e1fe88c2ea9c520b586445089
Brandon Ma <b2ma@uwaterloo.ca>
2018-03-21 11:18:22 -0400

    updated Transaction class

--- app/src/main/java/com/ba/cg/jn/tl/barter/Transaction.java
+++ app/src/main/java/com/ba/cg/jn/tl/barter/Transaction.java
@@ -1,56 +1,94 @@
 package com.ba.cg.jn.tl.barter;
 
+import java.util.ArrayList;
 /**
  * Created by tiffanylui on 2018-02-28.
  */
 
 public class Transaction {
-    private String targetUser;
-    private String id;
-    private String moneyValue;
-    private String barterItem;
-    private String moneyPerBarter;
+    private String name;
+    private String creatorId;
+    private ArrayList<String> targetUserIds;
+    private float cashValue;
+    private float barterValue;
+    private String barterUnit;
+    private boolean isActive;
+    private boolean isCompleted;
+    private String notes;
 
-    public Transaction() {
+    public Transaction(String name, String creatorId, ArrayList<String> targetUserIds, float cashValue, float barterValue, String barterUnit, boolean isActive, boolean isCompleted, String notes) {
+        this.name = name;
+        this.creatorId = creatorId;
+        this.targetUserIds = targetUserIds;
+        this.cashValue = cashValue;
+        this.barterValue = barterValue;
+        this.barterUnit = barterUnit;
+        this.isActive = isActive;
+        this.isCompleted = isCompleted;
+        this.notes = notes;
     }
 
-    public Transaction(String targetUser, String id, String moneyValue, String barterItem, String moneyPerBarter) {
-        this.targetUser = targetUser;
-        this.id = id;
-        this.moneyValue = moneyValue;
-        this.barterItem = barterItem;
-        this.moneyPerBarter = moneyPerBarter;
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void setBarterItem(String barterItem) {
-        this.barterItem = barterItem;
+    public void setCashValue(float cashValue){
+        this.cashValue = cashValue;
     }
 
-    public String getBarterItem() {
-        return barterItem;
+    public void setBarterValue(float barterValue){
+        this.barterValue = barterValue;
     }
 
-    public String getTargetUser() {
-        return targetUser;
+    public void setBarterUnit(String barterUnit){
+        this.barterUnit = barterUnit;
     }
 
-    public void setTargetUser(String targetUser) {
-        this.targetUser = targetUser;
+    public void setIsActive(boolean isActive){
+        this.isActive = isActive;
     }
 
-    public String getMoneyValue() {
-        return moneyValue;
+    public void setIsCompleted(boolean isCompleted){
+        this.isCompleted = isCompleted;
     }
 
-    public void setMoneyValue(String moneyValue) {
-        this.moneyValue = moneyValue;
+    public void setNotes(String notes){
+        this.notes = notes;
     }
 
-    public String getMoneyPerBarter() {
-        return moneyPerBarter;
+    public String getName(){
+        return name;
     }
 
-    public void setMoneyPerBarter(String moneyPerBarter) {
-        this.moneyPerBarter = moneyPerBarter;
+    public String getCreatorId(){
+        return creatorId;
+    }
+
+    public ArrayList<String> getTargetUserIds(){
+        return targetUserIds;
+    }
+
+    public float getCashValue(){
+        return cashValue;
+    }
+
+    public float getBarterValue(){
+        return barterValue;
+    }
+
+    public String getBarterUnit(){
+        return barterUnit;
+    }
+
+    public boolean getIsActive(){
+        return isActive;
+    }
+
+    public boolean getIsCompleted(){
+        return isCompleted;
+    }
+
+    public String getNotes(){
+        return notes;
     }
 }
